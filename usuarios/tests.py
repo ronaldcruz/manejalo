2c6dba7c4e7f56e3be3f9946f85c233b3914d821
Ronald Cruz <redcruzc@gmail.com>
2014-03-28 15:06:17 -0300

    Se añade test de prueba y notificacion de email para travis

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    
+    def test_basic_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_error(self):
+        self.assertTrue(False)
